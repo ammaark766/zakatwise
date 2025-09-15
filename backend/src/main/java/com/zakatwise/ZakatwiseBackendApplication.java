6fef6b951c1a4afd4084c5375e21b612f077c5ba
Ammaar <ammaark76@gmail.com>
2025-09-15 16:15:18 -0500

    feature/added-folder-and-backend-folders
    addded backend and frontend folders

---
+++ backend/src/main/java/com/zakatwise/ZakatwiseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.zakatwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZakatwiseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZakatwiseBackendApplication.class, args);
+	}
+
+}
